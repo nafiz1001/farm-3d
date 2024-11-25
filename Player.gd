9ab017f71de8fd6890121efce116690812cd7ecd
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-11-25 09:56:56 -0500

    prepare for clicking on 3d stuff

--- Player.gd
+++ Player.gd
@@ -2,8 +2,28 @@ extends CharacterBody3D
 
 @export var speed = 100
 
-func _input(_event):
-	pass
+func _input(event):
+	# TODO: move this logic into _physics_process()
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT :
+			# https://godotforums.org/d/33479-godot-4-raycasting-to-get-mouse-position-in-3d/3
+			var cam = $Camera
+			var ray_start = cam.project_ray_origin(event.position)
+			var ray_dir = cam.project_ray_normal(event.position)
+			var ray_end = ray_start + ray_dir * 100
+
+			var space_state = get_world_3d().direct_space_state
+			var query = PhysicsRayQueryParameters3D.create(ray_start, ray_end)
+
+			var result = space_state.intersect_ray(query)
+			if result:
+				var dot = $Dot.duplicate()
+				print("%s\t%s\t%s\t%s" % [event.position, ray_start, result.collider.get_path(), result.position])
+				dot.position = result.position
+				var parent = get_parent()
+				parent.add_child(dot)
+			else:
+				print("No collision")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,7 +42,7 @@ func _physics_process(delta: float) -> void:
 	if target_velocity.length_squared() > 0:
 		target_velocity = target_velocity.normalized()
 		self.velocity = target_velocity * speed * delta
-		look_at(self.position + Vector3(target_velocity.z, 0, -target_velocity.x))
+		$Pivot.look_at(self.position + Vector3(target_velocity.z, 0, -target_velocity.x))
 	else:
 		self.velocity = Vector3.ZERO
 	move_and_slide()
