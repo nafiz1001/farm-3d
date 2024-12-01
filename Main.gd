0304d74b5ba1544ebe698bc1f6ac6d8075e58074
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-12-01 11:18:27 -0500

    stuff

--- Main.gd
+++ Main.gd
@@ -9,12 +9,19 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_camera_click(result: Dictionary) -> void:
+	var root_node = get_tree().current_scene
 	var node: Node = result.collider
-	node = node.get_parent()
+	if node.get_parent() != root_node:
+		node = node.get_parent()
+	print(node)
 	if node.is_in_group("crop") or node.is_in_group("soil"):
 		character.interact(node)
 		var target_area = node.get_interaction_area()
 		target_area.outline = true
 		await get_tree().create_timer(0.1).timeout
 		target_area.outline = false
-	
+	else:
+		var soil := preload("res://soil.tscn").instantiate()
+		soil.position = result.position
+		soil.crop = preload("res://cabbage.tscn").instantiate()
+		root_node.add_child(soil)
